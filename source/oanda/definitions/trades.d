e2cf8bc3073a7ce50d9b7bf52f0b57ae6691f3a6
Tomáš Chaloupka <chalucha@gmail.com>
2016-05-22 22:56:43 +0200

    Definitions refactoring and completion - WIP

--- source/oanda/definitions/trades.d
+++ source/oanda/definitions/trades.d
@@ -3,7 +3,7 @@
 import oanda.definitions.primitives;
 import oanda.definitions.pricing : PriceValue;
 import oanda.definitions.account : AccountUnits;
-import oanda.definitions.orders : OrderID;
+import oanda.definitions.orders : OrderID, TakeProfitOrder, StopLossOrder, TrailingStopLossOrder;
 import oanda.definitions.transactions : ClientExtensions, TransactionID;
 
 /**
@@ -21,9 +21,41 @@ alias string TradeID;
  */
 alias string TradeSpecifier;
 
+/// The specification of a Trade within an Account. This includes the full representation of the Trade’s dependent Orders in addition to the IDs of those Orders.
 struct Trade
 {
-	//TODO
+	/// The Trade’s identifier, unique within the Trade’s Account.
+	TradeID id;
+	/// The Trade’s Instrument.
+	InstrumentName instrument;
+	/// The execution price of the Trade.
+	PriceValue price;
+	/// The date/time when the Trade was opened.
+	SysTime openTime;
+	/// The current state of the Trade.
+	TradeState state;
+	/// The initial size of the Trade. Negative values indicate a short Trade, and positive values indicate a long Trade.
+	DecimalNumber initialUnits;
+	/// The number of units currently open for the Trade. This value is reduced to 0.0 as the Trade is closed.
+	DecimalNumber currentUnits;
+	/// The total profit/loss realized on the closed portion of the Trade.
+	AccountUnits realizedPL;
+	/// The unrealized profit/loss on the open portion of the Trade.
+	AccountUnits unrealizedPL;
+	/// The IDs of the Transactions that have closed portions of this Trade.
+	TransactionID[] closingTransactionIDs;
+	/// The financing paid/collected for this Trade.
+	AccountUnits financing;
+	/// The date/time when the Trade was fully closed. Only provided for Trades whose state is CLOSED.
+	SysTime closeTime;
+	/// The client extensions of the Trade.
+	ClientExtensions clientExtensions;
+	/// Full representation of the Trade’s Take Profit Order, only provided if such an Order exists.
+	TakeProfitOrder takeProfitOrder;
+	/// Full representation of the Trade’s Stop Loss Order, only provided if such an Order exists.
+	StopLossOrder stopLossOrder;
+	/// Full representation of the Trade’s Trailing Stop Loss Order, only provided if such an Order exists.
+	TrailingStopLossOrder trailingStopLossOrder;
 }
 
 /// The summary of a Trade within an Account. This representation does not provide the full details of the Trade’s dependent Orders.
